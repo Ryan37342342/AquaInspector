79e61c9b02166019f635a8e1e59f350ddbc764ba
ryan <ryan.mccarthy@lonrix.com>
2024-10-31 21:57:01 +1300

    Add project files.

---
+++ Controllers/AquariumController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AquaInspector.Models;
+
+namespace AquaInspector.Controllers
+{
+    [Route("api/[controller]")]
+    [Controller]
+    public class AquariumController : ControllerBase
+    {
+        [HttpPost("temperature")]
+        public IActionResult PostTemperature([FromBody] TemperatureReading reading)
+        {
+            // Process and store temperature data
+            return Ok();
+        }
+    }
+}
---
+++ Models/TemperatureReading.cs
@@ -0,0 +1,17 @@
+﻿namespace AquaInspector.Models
+{
+    /// <summary>
+    /// A model class to hold the temperate of the fish tank 
+    /// </summary>
+    public class TemperatureReading
+    {
+        // ID of the Tank
+        public int TankID { get; set; }
+        // the recorded temperature
+        public double Temp { get; set; }
+        // the date the temp was recorded 
+        public DateTime TimeStamp { get; set; }
+
+        
+    }
+}
